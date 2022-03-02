a91429977e1259aa3967cafa5e7c520e2fe5853b
Luis Chavez <luichav3z@gmail.com>
2022-03-02 02:24:55 -0800

    signup/login work

---
+++ controllers/posts.js
@@ -0,0 +1,44 @@
+const Post = require('../models/post');
+
+const { v4: uuidv4 } = require("uuid");
+const S3 = require("aws-sdk/clients/s3");
+const s3 = new S3();
+
+const BUCKET = process.env.BUCKET_NAME;
+
+module.exports = {
+    create,
+    index,
+}
+
+function create(req, res){
+	const filePath = `${uuidv4()}${req.file.originalname}`;
+	const params = {Bucket: BUCKET, Key: filePath, Body: req.file.buffer}
+	s3.upload(params, async function(err, data){
+		if (err) return res.status(400).json({err})
+		try {
+			let post = await Post.create({
+				caption: req.body.caption,
+				location: req.body.location,
+				user: req.user,
+				photoUrl: data.Location
+			})
+
+			post = await post.populate('user')
+			res.status(201).json({post})
+
+		} catch(err){
+			console.log(err)
+			res.status(400).json({err})
+		}
+	})
+ }
+ 
+async function index(req, res) {
+	try {
+	  const posts = await Post.find({}).populate("user").exec();
+	  res.status(200).json({ posts: posts });
+	} catch (err) {
+	  res.status(400).json({ err });
+	}
+  }
---
+++ controllers/users.js
@@ -0,0 +1,68 @@
+const User = require('../models/user');
+const Post = require('../models/post');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+const { v4: uuidv4 } = require('uuid');
+const S3 = require('aws-sdk/clients/s3');
+const s3 = new S3();
+
+const BUCKET = process.env.BUCKET;
+
+module.exports = {
+  signup,
+  login,
+  profile
+};
+
+async function profile(req, res){
+  try{
+    const user = await User.findOne({username: req.params.username})
+    if(!user) return res.status(404).json({err: 'User not found'})
+    const posts = await Post.find({user: user._id}).populate('user').exec();
+    res.status(200).json({posts: posts, user: user})
+  }catch(err){
+    res.status(400).json({err})
+  }
+}
+
+function signup(req, res) {
+  const filePath = `${uuidv4()}/${req.file.originalname}`
+  const params = {Bucket: process.env.BUCKET_NAME, Key: filePath, Body: req.file.buffer};
+  s3.upload(params, async function(err, data){
+    console.log(data, 'from aws')
+    const user = new User({...req.body, photoUrl: data});
+    try {
+      await user.save();
+      const token = createJWT(user);
+      res.json({ token });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  })
+}
+
+async function login(req, res) {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    console.log(user, ' this user in login')
+    if (!user) return res.status(401).json({err: 'bad credentials'});
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (isMatch) {
+        const token = createJWT(user);
+        res.json({token});
+      } else {
+        return res.status(401).json({err: 'bad credentials'});
+      }
+    });
+  } catch (err) {
+    return res.status(401).json(err);
+  }
+}
+
+function createJWT(user) {
+  return jwt.sign(
+    {user},
+    SECRET,
+    {expiresIn: '24h'}
+  );
+}
